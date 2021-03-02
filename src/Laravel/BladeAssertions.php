d8337d7e70bff105dab9039c72af60f54e36fc95
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-02 12:55:20 +0100

    add blade assertion

---
+++ src/Laravel/BladeAssertions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Astrotomic\PhpunitAssertions\Laravel;
+
+use Gajus\Dindent\Indenter;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+trait BladeAssertions
+{
+    public static function assertRenderEquals(string $expected, string $template, array $data = []): void
+    {
+        $indenter = new Indenter();
+
+        PHPUnit::assertSame(
+            $indenter->indent($expected),
+            $indenter->indent((string) static::render($template, $data))
+        );
+    }
+
+
+    protected static function render(string $template, array $data = []): string
+    {
+        $tempDirectory = sys_get_temp_dir();
+
+        if (! in_array($tempDirectory, View::getFinder()->getPaths())) {
+            View::addLocation($tempDirectory);
+        }
+
+        $tempFile = tempnam($tempDirectory, 'laravel-blade').'.blade.php';
+
+        file_put_contents($tempFile, $template);
+
+        return View::make(Str::before(basename($tempFile), '.blade.php'), $data)->render();
+    }
+}
---
+++ tests/Laravel/BladeAssertionsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Astrotomic\PhpunitAssertions\Tests\Laravel;
+
+use Astrotomic\PhpunitAssertions\Laravel\BladeAssertions;
+
+final class BladeAssertionsTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider hundredTimes
+     */
+    public function it_can_validate_render(): void
+    {
+        $price = random_int(0, 10000) / 100;
+
+        $string = number_format($price, 2);
+
+        BladeAssertions::assertRenderEquals(
+            "<p>Price: <code>{$string} €</code></p>",
+            '<p>Price: <code>{{ number_format($price, 2) }} €</code></p>',
+            ['price' => $price]
+        );
+    }
+}
